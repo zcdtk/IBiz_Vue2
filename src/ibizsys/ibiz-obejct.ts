6fa1ce5b3d587b38c1ca342c08cc6d5aabd4809e
zcdtk <zcdtk@github.com>
2018-09-15 12:55:23 +0800

    update

--- src/ibizsys/ibiz-obejct.ts
+++ src/ibizsys/ibiz-obejct.ts
@@ -1,3 +1,4 @@
+import { observable, Subject, Observable } from 'rxjs';
 /**
  * 抽象接口基类
  *
@@ -14,6 +15,14 @@ export abstract class IBizObejct {
      * @memberof IBizObejct
      */
     public name: string;
+
+    /**
+     * 事件注册管理
+     *
+     * @type {Map<string, Subject<any>>}
+     * @memberof IBizObejct
+     */
+    public events: Map<string, Subject<any>> = new Map();
 
     /**
      * Creates an instance of IBizObejct.
@@ -44,5 +53,39 @@ export abstract class IBizObejct {
      */
     public getName(): string {
         return this.name;
+    }
+
+    /**
+     * 事件订阅
+     *
+     * @param {string} name
+     * @returns {Observable<any>}
+     * @memberof IBizObejct
+     */
+    public on(name: string): Observable<any> {
+        let subject: Subject<any>;
+        if (name && !Object.is(name, '')) {
+            if (!this.events.get(name)) {
+                subject = new Subject();
+                this.events.set(name, subject);
+            } else {
+                subject = this.events.get(name);
+            }
+            return subject.asObservable();
+        }
+    }
+
+    /**
+     * 事件抛出
+     *
+     * @param {string} name
+     * @param {*} data
+     * @memberof IBizObejct
+     */
+    public fire(name: string, data: any): void {
+        if (this.events.get(name)) {
+            const subject: Subject<any> = this.events.get(name);
+            subject.next(data);
+        }
     }
 }
