1ce745f0b7b4df7d8251bec0a4ce0371c7aeb142
zcdtk <zcdtk@github.com>
2018-09-17 10:39:42 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,4 @@
 var gulp = require('gulp');  // gulp 插件
-var typescript = require('gulp-typescript'); // gulp-typescript插件
-var tsProject = typescript.createProject('tsconfig.json');  // ts配置信息
 var concat = require('gulp-concat'); // 合并插件
 
 // 合成资源依赖文件（非压缩版）
@@ -12,29 +10,7 @@ gulp.task('assets', function () {
     ];
     return gulp.src(assets)
         .pipe(concat('assets.js'))
-        .pipe(gulp.dest('assets'));
+        .pipe(gulp.dest('dist'));
 });
 
-// 编译ts文件
-gulp.task('compilets', function () {
-    return tsProject.src()
-        .pipe(tsProject())
-        .js.pipe(gulp.dest('ibizsys'));
-});
-
-// 合并编译后的JavaScript文件（非压缩版）
-gulp.task('concatibizsys', function () {
-    // 目录（顺序）
-    var ibizsys = [
-        // 基础文件
-        'ibizsys/ibiz-obejct.js',
-        // 部件
-        'ibizsys/widget/ibiz-control.js',
-        // 控制器
-        'ibizsys/app/ibiz-view-controller.js'
-    ]
-    return gulp.src(ibizsys)
-        .pipe(concat('ibizsys.js'))
-        .pipe(gulp.dest('ibizsys'));
-});
 
