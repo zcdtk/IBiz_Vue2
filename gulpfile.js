54045847169fa669d9b87693070ea89415afbeb0
zcdtk <zcdtk@github.com>
2018-09-17 10:56:31 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,6 @@ var typescript = require('gulp-typescript'); // gulp-typescript插件
 var tsProject = typescript.createProject('tsconfig.json');  // ts配置信息
 var concat = require('gulp-concat'); // 合并插件
 
-// 合成资源依赖文件（非压缩版）
-gulp.task('assets', function () {
-    var assets = [
-        'node_modules/vue/dist/vue.js',
-        'node_modules/vue-router/dist/vue-router.js',
-        'node_modules/rxjs/Rx.js'
-    ];
-    return gulp.src(assets)
-        .pipe(concat('assets.js'))
-        .pipe(gulp.dest('assets'));
-});
-
 // 编译ts文件
 gulp.task('compilets', function () {
     return tsProject.src()
