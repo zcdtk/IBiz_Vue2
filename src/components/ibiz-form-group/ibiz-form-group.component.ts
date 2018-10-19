920dde49b08a503a885e6e1c0357c1b34263fdce
zcdtk <zcdtk@github.com>
2018-10-19 10:41:10 +0800

    update

--- src/components/ibiz-form-group/ibiz-form-group.component.ts
+++ src/components/ibiz-form-group/ibiz-form-group.component.ts
@@ -1,15 +1,24 @@
 Vue.component('ibiz-form-group', {
     template: `
-        <card :bordered="false" :dis-hover="true">
-            <p class="" slot="title"> {{ group.caption }}</p>
-            <row :gutter="10">
-                <slot></slot>
-            </row>
-        </card>
+        <div>
+            <template v-if="group.showCaption">
+                <card :bordered="false" :dis-hover="true">
+                    <p class="" slot="title"> {{ group.caption }}</p>
+                    <row :gutter="10">
+                        <slot></slot>
+                    </row>
+                </card>
+            </template>
+            <template v-else>
+                <row :gutter="10">
+                    <slot></slot>
+                </row>
+            </template>
+        </div>
     `,
-    props: ['form', 'group' , 'name'],
+    props: ['form', 'group', 'name'],
     data: function () {
-        let data: any = {};
+        let data: any = { };
         console.log(this.form);
         console.log(this.group);
         console.log(this.name);
