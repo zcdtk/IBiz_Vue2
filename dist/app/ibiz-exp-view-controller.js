78093197d209295c472cb0fb738e7856f1a06c15
zcdtk <zcdtk@github.com>
2018-10-22 18:13:09 +0800

    upidate

--- dist/app/ibiz-exp-view-controller.js
+++ dist/app/ibiz-exp-view-controller.js
@@ -1,67 +1,77 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    }
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 /**
  * 导航视图控制器
  *
  * @class IBizExpViewController
  * @extends {IBizMianViewController}
  */
-class IBizExpViewController extends IBizMianViewController {
-
+var IBizExpViewController = /** @class */ (function (_super) {
+    __extends(IBizExpViewController, _super);
     /**
      * Creates an instance of IBizExpViewController.
      * 创建 IBizExpViewController 实例
-     * 
+     *
      * @param {*} [opts={}]
      * @memberof IBizExpViewController
      */
-    constructor(opts: any = {}) {
-        super(opts);
-        let _this = this;
+    function IBizExpViewController(opts) {
+        if (opts === void 0) { opts = {}; }
+        var _this_1 = _super.call(this, opts) || this;
+        var _this = _this_1;
+        return _this_1;
     }
-
-    public init(opts:any = {}):void {
-        super.init(opts);
-        const expCtrl = this.getExpCtrl();
+    IBizExpViewController.prototype.init = function (opts) {
+        var _this_1 = this;
+        if (opts === void 0) { opts = {}; }
+        _super.prototype.init.call(this, opts);
+        var expCtrl = this.getExpCtrl();
         if (expCtrl) {
-            expCtrl.on(IBizTreeExpBar.SELECTIONCHANGE, (item) => {
-                this.onExpCtrlSelectionChange(item);
+            expCtrl.on(IBizTreeExpBar.SELECTIONCHANGE, function (item) {
+                _this_1.onExpCtrlSelectionChange(item);
             });
-
             // expCtrl.on(IBizTreeExpBar.LOADED, (item) => {
             //     this.onExpCtrlLoaded(item);
             // });
         }
-    }
-
+    };
     /**
      * 视图部件初始化
      *
      * @memberof IBizExpViewController
      */
-    public onInit(): void {
-
-    }
-
+    IBizExpViewController.prototype.onInit = function () {
+    };
     /**
      * 初始化导航部件
-     * 
+     *
      * @memberof IBizExpViewController
      */
-    public onInitComponents(): void {
-        
-    }
-
+    IBizExpViewController.prototype.onInitComponents = function () {
+    };
     /**
      * 导航部件加载
-     * 
+     *
      * @memberof IBizExpViewController
      */
-    public onLoad(): void {
-        const expCtrl = this.getExpCtrl();
+    IBizExpViewController.prototype.onLoad = function () {
+        var expCtrl = this.getExpCtrl();
         if (expCtrl) {
             expCtrl.load({});
         }
-    }
-
+    };
     /**
      * 视图销毁
      *
@@ -73,100 +83,92 @@ class IBizExpViewController extends IBizMianViewController {
     //         this.$expRouteRvents.unsubscribe();
     //     }
     // }
-
     /**
      * 获取导航部件
-     * 
-     * @returns {*} 
+     *
+     * @returns {*}
      * @memberof IBizExpViewController
      */
-    public getExpCtrl(): any {
-        let expctrl = this.getExpBar();
+    IBizExpViewController.prototype.getExpCtrl = function () {
+        var expctrl = this.getExpBar();
         if (expctrl) {
             return expctrl;
         }
-
         expctrl = this.getExpTab();
         if (expctrl) {
             return expctrl;
         }
-
         return undefined;
-    }
-
+    };
     /**
      * 获取导航部件
-     * 
-     * @returns {*} 
+     *
+     * @returns {*}
      * @memberof IBizExpViewController
      */
-    public getExpBar(): any {
+    IBizExpViewController.prototype.getExpBar = function () {
         return this.getControl('expbar');
-    }
-
+    };
     /**
      * 获取导航分页部件
-     * 
-     * @returns {*} 
+     *
+     * @returns {*}
      * @memberof IBizExpViewController
      */
-    public getExpTab(): any {
+    IBizExpViewController.prototype.getExpTab = function () {
         return this.getControl('exptab');
-    }
-
+    };
     /**
      * 导航部件值选中变化
      *
      * @param {*} [item={}]
      * @memberof IBizExpViewController
      */
-    public onExpCtrlSelectionChange(item: any = {}): void {
-
-    }
-
+    IBizExpViewController.prototype.onExpCtrlSelectionChange = function (item) {
+        if (item === void 0) { item = {}; }
+    };
     /**
      * 导航树部件加载完成
      *
      * @param {*} [item={}]
      * @memberof IBizExpViewController
      */
-    public onExpCtrlLoaded(item: any = {}): void {
-
-    }
-
+    IBizExpViewController.prototype.onExpCtrlLoaded = function (item) {
+        if (item === void 0) { item = {}; }
+    };
     /**
      * 获取导航项视图参数，在发布视图控制器内重写
-     * 
-     * @param {*} [arg={}] 
-     * @returns {*} 
+     *
+     * @param {*} [arg={}]
+     * @returns {*}
      * @memberof IBizExpViewController
      */
-    public getExpItemView(arg: any = {}): any {
+    IBizExpViewController.prototype.getExpItemView = function (arg) {
+        if (arg === void 0) { arg = {}; }
         return undefined;
-    }
-
+    };
     /**
      * 获取新建导航视图参数，在发布视图控制器中重写
-     * 
-     * @param {*} [arg={}] 
-     * @returns {*} 
+     *
+     * @param {*} [arg={}]
+     * @returns {*}
      * @memberof IBizExpViewController
      */
-    public getNewDataView(arg: any = {}): any {
+    IBizExpViewController.prototype.getNewDataView = function (arg) {
+        if (arg === void 0) { arg = {}; }
         return undefined;
-    }
-
+    };
     /**
      * 获取编辑导航视图参数，在发布视图控制器中重写
-     * 
-     * @param {*} [arg={}] 
-     * @returns {*} 
+     *
+     * @param {*} [arg={}]
+     * @returns {*}
      * @memberof IBizExpViewController
      */
-    public getEditDataView(arg: any = {}): any {
+    IBizExpViewController.prototype.getEditDataView = function (arg) {
+        if (arg === void 0) { arg = {}; }
         return undefined;
-    }
-
+    };
     /**
      * 节点路由是否存在
      *
@@ -174,25 +176,24 @@ class IBizExpViewController extends IBizMianViewController {
      * @returns {boolean}
      * @memberof IBizExpViewController
      */
-    public hasRoute(routeLink: string): boolean {
-        let hasRoute = false;
+    IBizExpViewController.prototype.hasRoute = function (routeLink) {
+        var hasRoute = false;
         // if (this.$routeActive && this.$routeActive.routeConfig && this.$routeActive.routeConfig.children !== null) {
         //     const index: number = this.$routeActive.routeConfig.children.findIndex(item => Object.is(item.path, routeLink));
         //     hasRoute = (index !== -1) ? true : false;
         // }
         return hasRoute;
-    }
-
+    };
     /**
      * 是否需要手动跳转路由
-     * 
+     *
      * @private
-     * @param {*} [item={}] 
-     * @returns {boolean} 
+     * @param {*} [item={}]
+     * @returns {boolean}
      * @memberof IBizTreeExpViewController
      */
-    public isRefreshView(routeSting: string): boolean {
-        let refreshView = false;
+    IBizExpViewController.prototype.isRefreshView = function (routeSting) {
+        var refreshView = false;
         // if (this.$routeActive && this.$routeActive.children && this.$routeActive.children.length > 0) {
         //     const arr = this.$routeActive.children[0];
         //     if (Object.is(arr.routeConfig.path, routeSting.toLowerCase())) {
@@ -200,8 +201,7 @@ class IBizExpViewController extends IBizMianViewController {
         //     }
         // }
         return refreshView;
-    }
-
+    };
     /**
      * 打开导航子视图
      *
@@ -209,28 +209,29 @@ class IBizExpViewController extends IBizMianViewController {
      * @returns {void}
      * @memberof IBizExpViewController
      */
-    public openExpChildView(item: any = {}): void {
+    IBizExpViewController.prototype.openExpChildView = function (item) {
+        if (item === void 0) { item = {}; }
         if (!item || Object.keys(item).length === 0) {
             return;
         }
-        const view = this.getExpItemView(item.expitem);
+        var view = this.getExpItemView(item.expitem);
         if (!view) {
             return;
         }
-        const hasRouter: boolean = this.hasRoute(view.routelink);
+        var hasRouter = this.hasRoute(view.routelink);
         if (!hasRouter) {
             return;
         }
-
-        let data: any = {};
+        var data = {};
         Object.assign(data, item.expitem.viewparam);
         if (this.isRefreshView(view.routelink)) {
             Object.assign(data, { refreshView: true });
         }
-        const exp = this.getExpBar();
+        var exp = this.getExpBar();
         if (exp) {
             exp.setSelectItem(item);
         }
         // this.openView(view.routelink, data);
-    }
-}
+    };
+    return IBizExpViewController;
+}(IBizMianViewController));
