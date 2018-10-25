8c958d170451e2581cd6c37c664e61f4f09ebd33
zcdtk <zcdtk@github.com>
2018-10-26 00:33:32 +0800

    update

--- dist/components/ibiz-picker/ibiz-picker.component.js
+++ dist/components/ibiz-picker/ibiz-picker.component.js
@@ -1,6 +1,6 @@
 "use strict";
 Vue.component('ibiz-picker', {
-    template: "\n    <i-input placeholder=\"Enter text\" style=\"width: auto\">\n        <icon type=\"ios-search\" slot=\"suffix\" />\n    </i-input>\n    ",
+    template: "\n        <i-input icon=\"ios-search\" v-model=\"field.value\" \n        :disabled=\"field.disabled\" @on-search=\"onSearch\"></i-input>\n    ",
     props: ['field', 'name', 'modalviewname'],
     data: function () {
         var data = {};
@@ -9,8 +9,22 @@ Vue.component('ibiz-picker', {
     mounted: function () {
     },
     methods: {
-        onSelect: function (name) {
-            console.log('1231231231');
-        }
+        onSearch: function () {
+            console.log();
+            var data = {
+                height: 0,
+                layout: "fit",
+                maximizable: true,
+                modal: true,
+                modalviewname: "org-pickup-view_modalview",
+                openMode: "POPUPMODAL",
+                title: "产品编辑视图（vue)",
+                viewname: "product-edit-view-vue",
+                viewparam: {},
+                viewurl: "/pages/common/product-edit-view-vue/product-edit-view-vue.html#/product-edit-view-vue",
+                width: 0
+            };
+            this.$root.addModal(data);
+        },
     }
 });
--- src/components/ibiz-picker/ibiz-picker.component.ts
+++ src/components/ibiz-picker/ibiz-picker.component.ts
@@ -1,8 +1,7 @@
 Vue.component('ibiz-picker', {
     template: `
-    <i-input placeholder="Enter text" style="width: auto">
-        <icon type="ios-search" slot="suffix" />
-    </i-input>
+        <i-input icon="ios-search" v-model="field.value" 
+        :disabled="field.disabled" @on-search="onSearch"></i-input>
     `,
     props: ['field', 'name', 'modalviewname'],
     data: function () {
@@ -13,8 +12,22 @@ Vue.component('ibiz-picker', {
         
     },
     methods: {
-        onSelect(name) {
-            console.log('1231231231');
-        }
+        onSearch(){
+            console.log();
+            let data = {
+                height: 0,
+                layout: "fit",
+                maximizable: true,
+                modal: true,
+                modalviewname: "org-pickup-view_modalview",
+                openMode: "POPUPMODAL",
+                title: "产品编辑视图（vue)",
+                viewname: "product-edit-view-vue",
+                viewparam: {  },
+                viewurl: "/pages/common/product-edit-view-vue/product-edit-view-vue.html#/product-edit-view-vue",
+                width: 0
+            };
+            this.$root.addModal(data);
+        },
     }
 });
