ad23bb978bba29db01907dde568767973c859251
zcdtk <zcdtk@github.com>
2018-11-01 15:45:29 +0800

    update

--- src/app/ibiz-edit-view-controller.ts
+++ src/app/ibiz-edit-view-controller.ts
@@ -638,7 +638,7 @@ class IBizEditViewController extends IBizMainViewController {
         if (parentWindow) {
             let pWinIBizApp: IBizApp = parentWindow.getIBizApp();
             let viewparam = this.getViewParam();
-            pWinIBizApp.fireRefreshView({ openerid: viewparam.openerid, viewUsage: this.getViewUsage() });
+            pWinIBizApp.fireRefreshView({ openerid: viewparam.openerid, viewUsage: viewparam.viewUsage });
         }
         if (this.isModal()) {
             let result: any = { ret: 'OK', activeData: this.getForm().getValues() };
